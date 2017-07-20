d231670e39681edb98f425278b124cf41e2ea9b7
Christopher Mlalazi <chrismlalazi@gmail.com>
2017-07-19 19:46:13 -0500

    VueJs

---
+++ js/vue.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+  	//Array of animals
+     
+    title: 'Looping through a list of cars with key and value pairs',
+    //Array of cars with object with keys and values
+    cars: [
+    {name: 'BMW', age: 5, color: 'black'},
+    {name: 'Merc', age: 'unknown', color: 'metallic'}
+    ], 
+
+    name: 'Looping through the BIG FIVE animals in Africa and including indices',
+
+     animals: ['Lion', 'Elephant', 'Leopard', 'Rhinocerous', 'Buffalo'],
+
+     numbers: 'Iterating through a list of numbers',
+  }
+});
